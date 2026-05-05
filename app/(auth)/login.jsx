e051da7afa9f50bd625a360686b274b9b618ee84
konstantin <konstantindobrev576@gmail.com>
2026-05-05 14:55:04 +0200

    chore: initialize project with package.json for habit tracker frontend

---
+++ app/(auth)/login.jsx
@@ -0,0 +1,62 @@
+import { StyleSheet, Pressable, Text } from 'react-native'
+import { Link } from 'expo-router'
+import { Colors } from '../../constants/Colors'
+
+//themed components
+import ThemedView from '../../components/ThemedView'
+import ThemedText from '../../components/ThemedText'
+import Spacer from '../../components/Spacer'
+import ThemedButton from '../../components/ThemedButton'
+
+const Login = () => {
+
+  const handleSubmit = () => {
+    console.log('Login button pressed')
+  }
+
+  return (
+    <ThemedView style={styles.container}>
+      <Spacer />
+      <ThemedText title={true} style={styles.title}>
+        Login to your account
+      </ThemedText>
+
+      <ThemedButton onPress={handleSubmit}>
+        <Text style={{ color: '#f2f2f2' }}>
+          Login
+        </Text>
+      </ThemedButton>
+
+      <Spacer height={100} />
+      <Link href="/register" style={styles.link}>
+        <ThemedText style={{ textAlign: 'center'}}>
+            Create an account
+        </ThemedText>
+      </Link>
+
+    </ThemedView>
+  )
+}
+
+export default Login
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 18,
+        marginBottom: 30,
+    },
+    btn: { 
+        backgroundColor: Colors.primary,
+        padding: 15,
+        borderRadius: 5,
+    },
+    pressed: {
+        opacity: 0.8,
+    },
+})
